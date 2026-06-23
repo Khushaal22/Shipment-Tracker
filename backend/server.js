cce5f2dcd41df62842760c97b58e1c81aeff7c9d
Khushaal22 <140880425+Khushaal22@users.noreply.github.com>
2026-06-23 21:39:36 +0530

    backend init db connected

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const authRoutes = require('./routes/authRoutes');
+const shipmentRoutes = require('./routes/shipmentRoutes')
+
+const app = express();
+
+connectDB();
+
+app.use(cors())
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+    res.json({ status: 'OK', message: 'server is running' });
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/shipments', shipmentRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
