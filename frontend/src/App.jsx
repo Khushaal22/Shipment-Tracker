733ec7ab89d7697f292f644116a4316991c8ee07
Khushaal22 <140880425+Khushaal22@users.noreply.github.com>
2026-06-25 00:28:27 +0530

    feat: Shipment creation frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,13 @@ import RoleRoute from './components/RoleRoute';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Unauthorized from './pages/Unauthorized';
+import SenderDashboard from './pages/sender/dashboard';
+import CreateShipment from './pages/sender/createShipment';
+import MyShipments from './pages/sender/MyShipments';
+import ShipmentDetail from './pages/sender/ShipmentDetail';
+
 
 // Placeholder dashboards — filled out in later phases
-function SenderDashboard() {
-  return <h2>Sender Dashboard — Phase 2 builds this</h2>;
-}
 function TrackerDashboard() {
   return <h2>Tracker Dashboard — Phase 3 builds this</h2>;
 }
@@ -31,17 +33,37 @@ export default function App() {
             path="/sender/dashboard"
             element={
               <RoleRoute allowedRoles={['sender']}>
-                  <SenderDashboard />
+                <SenderDashboard />
               </RoleRoute>
             }
           />
+          <Route
+            path="/sender/create-shipment"
+            element={
+              <RoleRoute allowedRoles={['sender']}>
+                <CreateShipment />
+              </RoleRoute>
+            } />
+          <Route
+            path="/sender/my-shipments"
+            element={
+              <RoleRoute allowedRoles={['sender']}>
+                <MyShipments />
+              </RoleRoute>
+            } />
+          <Route
+            path="/sender/shipment/:id"
+            element={
+              <RoleRoute allowedRoles={['sender']}>
+                <ShipmentDetail /></RoleRoute>
+            } />
 
           {/* Tracker only */}
           <Route
             path="/tracker/dashboard"
             element={
               <RoleRoute allowedRoles={['tracker']}>
-                  <TrackerDashboard />
+                <TrackerDashboard />
               </RoleRoute>
             }
           />
@@ -51,7 +73,7 @@ export default function App() {
             path="/admin/dashboard"
             element={
               <RoleRoute allowedRoles={['admin']}>
-                  <AdminDashboard />
+                <AdminDashboard />
               </RoleRoute>
             }
           />
